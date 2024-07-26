bba6e166b3c754a71910a53978e982f43e3b9167
Anton Avtamonov <anton.avtamonov@vecima.com>
2024-07-26 00:34:37 -0400

    Comtinue to work on dashboard screen

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -2,9 +2,23 @@
 
 const auth_token_item = "auth_token"
 
+accountConfiguration = {
+    conversionRate: 100
+}
+
 function createAccount(email, password, dob, callback) {
     window.sessionStorage.setItem(auth_token_item, "test")
 
+    callback(null)
+}
+
+function orderCard(firstName, lastName, callback) {
+    accountConfiguration.card = {
+        firstName: firstName,
+        lastName: lastName,
+        expiration: "2/26",
+        balance: 100,
+    }
     callback(null)
 }
 
@@ -21,4 +35,51 @@ function logOut(callback) {
 
 function getToken() {
     return window.sessionStorage.getItem(auth_token_item)
+}
+
+function getConfiguration() {
+    return accountConfiguration
+}
+
+
+function validateInputField(elementId, statusElementId, validator) {
+    element = document.getElementById(elementId)
+    statusElement = document.getElementById(statusElementId)
+
+    isValid = false
+
+    if (element.value == "") {
+      statusElement.classList.remove("bad")
+      statusElement.classList.remove("good")
+    } else {
+      if (validator(element.value)) {
+        statusElement.classList.remove("bad")
+        statusElement.classList.add("good")
+
+        isValid = true
+      } else {
+        statusElement.classList.remove("good")
+        statusElement.classList.add("bad")
+      }
+    }
+
+    return isValid
+}
+
+function setElementEnabled(elementName, isEnabled) {
+    element = document.getElementById(elementName)
+    if (isEnabled) {
+        element.removeAttribute("disabled")
+    } else {
+        element.setAttribute("disabled", "")
+    }
+}
+
+function setElementValid(elementName, isValid) {
+    element = document.getElementById(elementName)
+    if (isValid) {
+        element.classList.remove("invalid")
+    } else {
+        element.classList.add("invalid")
+    }
 }
